0d36d5a5379695b541727cf79116184030c4e45c
Jaewook Jung <24-091@ksa.hs.kr>
2024-10-22 16:01:07 +0900

    Finished Agent

--- configf.py
+++ configf.py
@@ -4,7 +4,13 @@ config = {
     'C' : 2,
     'EPSILON' : 0.5,
     'ALPHA' : 0.9,
-    'MCTS_SEARCHES' : 1000
+    'MCTS_SEARCHES' : 1000,
+    'LearningRate' : 0.001,
+    'TEMPERATURE' : 0.8,
+    'BatchSize' : 64,
+    'Iterations' : 10,
+    'GamesPerIteration' : 128,
+    'Epochs' : 10,
 }
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
